cb15347d3a63ddce71ac28cd97340c983ad7dd1c
Jarukit Boonkong <6710110548@psu.ac.th>
2025-02-13 14:37:45 +0700

    fix: navigate each tour card to overview

--- client/src/Component/homepage/TourCard.js
+++ client/src/Component/homepage/TourCard.js
@@ -1,13 +1,18 @@
 import { Card } from 'antd';
-
+import {useNavigate} from 'react-router-dom';
 const { Meta } = Card;
 
 const TourCard = ({tour}) => {
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate(`/Trip/`);
+    };
     return (
         <Card
             hoverable
             style={{ width: 240 }}
             cover={<img alt="image" src={tour.image} />}
+            onClick={handleClick}
         >
             <Meta title={tour.name} description={tour.description} />
         </Card>
