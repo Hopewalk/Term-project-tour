bb819c5099a8c0ea26312f9b47801428e6316f83
Hope <oab084@gmail.com>
2025-02-26 01:43:14 +0700

    fix register

--- client/src/Auth/Register.js
+++ client/src/Auth/Register.js
@@ -9,6 +9,8 @@ const Register = () => {
     username: "",
     email: "",
     password: "",
+    first_name: "",
+    last_name: "",
   });
 
   const handleChange = (e) => {
@@ -26,53 +28,68 @@ const Register = () => {
   };
 
   return (
-    <div className="login-wrapper">
-      <div className="login-container">
-        <div className="login-left">
-          <form className="login-form" onSubmit={handleSubmit}>
-            <h2>สมัครเพื่อเข้าสู่ระบบ</h2>
-            <div className="form-group">
-              <label htmlFor="username">Username</label>
-              <input
-                type="text"
-                id="username"
-                value={form.username}
-                onChange={handleChange}
-                placeholder="Enter your username"
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="email">Email Address</label>
-              <input
-                type="email"
-                id="email"
-                value={form.email}
-                onChange={handleChange}
-                placeholder="Enter your email"
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                id="password"
-                value={form.password}
-                onChange={handleChange}
-                placeholder="Enter your password"
-                required
-              />
-            </div>
-            <button type="submit" className="login-button">
-              Sign Up
-            </button>
-          </form>
+    <div className="login-left">
+      <form className="login-form" onSubmit={handleSubmit}>
+        <h2>สมัครเพื่อเข้าสู่ระบบ</h2>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={form.username}
+            onChange={handleChange}
+            placeholder="Enter your username"
+            required
+          />
         </div>
-        <div className="login-right">
-          <img src="./Images/loginpic.jpg" alt="Login" />
+        <div className="form-group">
+          <label htmlFor="email">Email Address</label>
+          <input
+            type="email"
+            id="email"
+            value={form.email}
+            onChange={handleChange}
+            placeholder="Enter your email"
+            required
+          />
         </div>
-      </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={form.password}
+            onChange={handleChange}
+            placeholder="Enter your password"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="first_name">First name</label>
+          <input
+            type="text"
+            id="first_name"
+            value={form.first_name}
+            onChange={handleChange}
+            placeholder="Enter your First name"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="last_name">Last name</label>
+          <input
+            type="text"
+            id="last_name"
+            value={form.last_name}
+            onChange={handleChange}
+            placeholder="Enter your last_name"
+            required
+          />
+        </div>
+        <button type="submit" className="login-button">
+          Sign Up
+        </button>
+      </form>
     </div>
   );
 };
